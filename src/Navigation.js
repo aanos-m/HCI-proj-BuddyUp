e8ff40064b4ac53830efb4c7382cacb8797345fe
aanos-m <aanosmahmood@gmail.com>
2023-11-02 22:10:13 -0500

    fixed navigation bar and added mui icons

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,29 +1,69 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
 
-const Navigation = () => {
+const Navigation = ( 
+  { item1, item2, item3, item4}
+) => {
   return (
-    <div>
-    <Navbar bg="dark" expand="lg" className="bg-body-tertiary ">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">
-                <Nav.Link >Home</Nav.Link>
-            </Link>
-            <Link to="/about">
-                <Nav.Link >About</Nav.Link>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div 
+        style={{
+        padding: '5px',
+        margin: '5px',
+        width: '390px',
+        justifyContent: 'center',
+        alignContent: 'center',
+        borderRadius: '10px',
+        background: 'white',
+        boxShadow: '0px -4px 4px 0px rgba(0, 0, 0, 0.25)'
+    }}>
+      <ul class="nav nav-pills" style={{ justifyContent: 'space-around', fontSize: '16px'}}>
+        <li class="nav-item">
+            <button class="nav-link " 
+                    style={{ display: 'flex', 
+                            color: {item1},
+                              flexDirection: 'column',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              height: '50px'}}>
+              <HomeIcon fontSize="small" /> Home </button>
+        </li>
+        <li class="nav-item" >
+            <button class="nav-link " 
+                    style={{ display: 'flex',
+                            color: {item2},
+                              flexDirection: 'column',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              height: '50px'}}>
+              <PeopleIcon fontSize="small" /> Friends</button>
+        </li>
+        <li class="nav-item"  >
+            <button class="nav-link " 
+                    style={{ display: 'flex', 
+                    color: {item3},
+                              flexDirection: 'column',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              height: '50px'}}>
+              <SearchIcon fontSize="small"/> Search </button>
+        </li>
+        <li class="nav-item" >
+            <button class="nav-link " 
+                    style={{ display: 'flex', 
+                  color: {item4},
+                              flexDirection: 'column',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              height: '50px'}}>
+              <AccountCircleIcon fontSize="small"/> Account </button>
+        </li>
+       
+      </ul>
     </div>
   )
 }
 
-export default Navigation
+export default Navigation;
--- src/NewAccountPages/NewAccount.js
+++ src/NewAccountPages/NewAccount.js
@@ -1,9 +1,29 @@
-import React from 'react'
 import './NewAccount.css';
+import BuddyUp from "../components/images/BuddyUp.png";
+import Navigation from '../Navigation';
+
 
 const NewAccount = () => {
   return (
-    <div>NewAccount</div>
+    <>
+   
+    <div>
+      <img className="image" alt="BuddyUp" src={BuddyUp} />
+      <div id='screenDiv'>
+        <h1 id='textWrapper'>
+          It’s time for 
+          you to connect with people!!
+        </h1>
+      </div>
+    </div>
+    
+    <div style={{
+      position: 'absolute',
+      bottom: '0px'
+    }}>
+      <Navigation/>
+    </div>
+    </>
   )
 }
 
