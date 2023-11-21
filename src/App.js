9e4ee07fc5569ccb7b693dc9c7ffe6162e01b088
aanos-m <aanosmahmood@gmail.com>
2023-11-20 22:25:55 -0600

    character limit and go back on accCreate

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
     major: '',
     minor: '',
     likes: '',
+    aboutMe: ''
   });
 
 
--- src/CreateAccount/CreateAccount.js
+++ src/CreateAccount/CreateAccount.js
@@ -7,16 +7,41 @@ import Me from "../components/images/Me.png";
 import { useNavigate } from 'react-router-dom';
 
 const CreateAccount = ({ newUserObj, setNewUserObj }) => {
+  const MAX_CHARACTER_LIMIT_AGE = 2;
+  const MAX_CHARACTER_LIMIT_ABOUT_ME = 200
 
   const handleInputChange = (field, value) => {
-    setNewUserObj((prevObj) => ({ ...prevObj, [field]: value }));
+    let maxCharacterLimit;
+    switch (field) {
+      case 'age':
+        maxCharacterLimit = MAX_CHARACTER_LIMIT_AGE;
+        break;
+      case 'aboutMe':
+        maxCharacterLimit = MAX_CHARACTER_LIMIT_ABOUT_ME;
+        break;
+      default:
+        maxCharacterLimit = 25; // No character limit for other fields
+    }
+    if (value.length <= maxCharacterLimit) {
+      setNewUserObj((prevObj) => ({ ...prevObj, [field]: value }));
+    }
   };
 
   const [isOpen, setIsOpen] = useState(false);
 
+
   let navigate = useNavigate(); 
   const routeChange = () =>{ 
-    let path = `/NewAccount/Home`; 
+    if (newUserObj.name && newUserObj.year && newUserObj.age && newUserObj.major && newUserObj.minor && newUserObj.likes && newUserObj.aboutMe) {
+      let path = `/NewAccount/Home`;
+      navigate(path);
+    } else {
+      // Display an error message or handle it as needed
+      alert("Please fill in all fields.");
+    }
+  }
+  const routeBack = () =>{ 
+    let path = `/`; 
     navigate(path);
   }
 
@@ -32,36 +57,42 @@ const CreateAccount = ({ newUserObj, setNewUserObj }) => {
             <div role='button' onClick={()=>setIsOpen(!isOpen)} id='toggleBtn'> Toggle </div>
           </div>
 
-            <input className="text-wrapper-2" type="text" placeholder="Name"
+            <input className="text-wrapper-2" type="text" placeholder="Name" value={newUserObj.name}
               onChange={(e) => handleInputChange('name', e.target.value)}/>
 
-            <input className="text-wrapper-2" type="text" placeholder="Year"
+            <input className="text-wrapper-2" type="text" placeholder="Year" value={newUserObj.year}
             onChange={(e) => handleInputChange('year', e.target.value)}/>
 
-            <input className="text-wrapper-2" type="text" placeholder="Age"
+            <input className="text-wrapper-2" type="text" placeholder="Age" value={newUserObj.age}
             onChange={(e) => handleInputChange('age', e.target.value)}
             />
 
-            <input className="text-wrapper-2" type="text" placeholder="Major"
+            <input className="text-wrapper-2" type="text" placeholder="Major" value={newUserObj.major}
             onChange={(e) => handleInputChange('major', e.target.value)}
             />
 
-            <input className="text-wrapper-2" type="text" placeholder="Minor"
+            <input className="text-wrapper-2" type="text" placeholder="Minor" value={newUserObj.minor}
             onChange={(e) => handleInputChange('minor', e.target.value)}
             />
 
-            <input className="text-wrapper-2" type="text" placeholder="Likes"
+            <input className="text-wrapper-2" type="text" placeholder="Likes" value={newUserObj.likes}
             onChange={(e) => handleInputChange('likes', e.target.value)}
             />
 
-            <textarea  className="text-wrapper-2" type="text" placeholder="About Me" wrap='soft'
+            <textarea  className="text-wrapper-2" type="text" placeholder="About Me" wrap='soft' value={newUserObj.aboutMe}
+            onChange={(e) => handleInputChange('aboutMe', e.target.value)}
             style={{overflow:'hidden', resize:'none'}}/>
 
         </div>
         
         <div className="frame-4" role='button' onClick={routeChange}>
-          <div className="text-wrapper-3">Create</div>
+          <p className="text-wrapper-3">Create</p>
         </div>
+
+        <div className="frame-5" role='button' onClick={routeBack}>
+          <p className="text-wrapper-3">Back</p>
+        </div>
+
       </div>
     </div>
   );
